5319c438f908077cb3d5930512c40864a2318950
Jeremy Haakma <jchaakma@gmail.com>
2019-11-21 17:24:21 +1300

    Moved replies to json file

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json');
+var replies = require('./replies.json')
 var logger = require('./logger')
 //logger.setLogLevel("DEBUG")
 
@@ -9,73 +10,54 @@ client.on('ready', function (evt) {
     logger.info('Logged in as %s', client.user.tag)
 });
 
-var responses = [
-    "It just works!™️",
-    "Yes, I was in the chess club.",
-    "Sixteen times the detail!",
-    "See that mountain? you can climb it!",
-    "I am Todd Howard -- T-O-D-D H-O-W-A-R-D.",
-    "These NPCs are not scripted!",
-    "Sometimes, it doesn't just work."
-]
-function getDefaultResponse() {
-    return responses[Math.floor(Math.random() * responses.length)]
+//helper functions
+function getDefaultReply() {
+    return replies.defaultReplies[Math.floor(Math.random() * replies.defaultReplies.length)]
 }
-
 function hasText(message, pattern) {
     return message.content.toLowerCase().includes(pattern)
 }
-
-function isText(message, text) {
-    return message.content.toLowerCase === text
-}
-
-var replies = {
-    "it just works": "Sometimes, it doesn't just work.",
-    "tod howard": "I am Todd Howard -- T-O-D-D H-O-W-A-R-D.",
-    "tell me lies": "Tell me sweet little lies..."
-}
-
-var emojiReplies = {
-    "excited": [":excited:481404244239581205"],
-    "free the crab": [":banhammer:537248733155295243", "🦀"],
-    "caius cosades": [":sexy:527994270880235539"],
-    "feature creep": [":creeper:534834891704238112"],
-    "facepalm": [":facepalm:541813012063846402"],
+function sendReply(message, reply) {
+    //handle special logic of replies as commands
+    if (reply.startsWith('`')) {
+        message.channel.send(eval(reply))
+    } else {
+        message.channel.send(reply)    
+    }
 }
 
 client.on('message', message => {
     var validMessage = (
-        message.author.username != client.user.username //&&
-        //message.channel.parent.name == "General Talk"
+        message.author.username != client.user.username
     )
 
     if ( validMessage ) {
         var response
-
         //Default
         if ( hasText(message, "todd howard") ) {
-            response = getDefaultResponse()
+            response = getDefaultReply()
         }
         //Check prompts for replies
         else {
-            for ( prompt in replies ) {
+            for ( prompt in replies.textReplies ) {
                 if ( hasText(message, prompt) ) {
-                    response = replies[prompt]
+                    response = replies.textReplies[prompt]
                     break
                 }
             }
         }
         //Respond
         if ( response != null ) {
-            message.channel.send(response)
-            logger.debug("Response sent\n\tChannel: %s\n\tMessage: %s", message.channel.name, response)
+            response.forEach( function(reply) {
+                sendReply(message, reply)
+                logger.debug("Response sent\n\tChannel: %s\n\tMessage: %s", message.channel.name, reply)
+            })
         }
 
         //Check prompts for reactions
-        for ( prompt in emojiReplies ) {
+        for ( prompt in replies.emojiReplies ) {
             if ( hasText(message, prompt ) ) {
-                emojiReplies[prompt].forEach( function(emoji) {
+                replies.emojiReplies[prompt].forEach( function(emoji) {
                     message.react(emoji)
                 })
             }
