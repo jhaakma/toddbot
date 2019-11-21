0ca7ce6daf1d6d9da4d5d7da2cdd0a97056a9bdf
Jeremy Haakma <jchaakma@gmail.com>
2019-11-21 18:03:54 +1300

    added fishystick, refactored code

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ var Discord = require('discord.js');
 var auth = require('./auth.json');
 var replies = require('./replies.json')
 var logger = require('./logger')
-//logger.setLogLevel("DEBUG")
+logger.setLogLevel("DEBUG")
 
 // Initialize Discord client
 var client = new Discord.Client();
@@ -17,6 +17,9 @@ function getDefaultReply() {
 function hasText(message, pattern) {
     return message.content.toLowerCase().includes(pattern)
 }
+function startsWith(message, pattern) {
+    return message.content.toLowerCase().startsWith(pattern)
+}
 function sendReply(message, reply) {
     //handle special logic of replies as commands
     if (reply.startsWith('`')) {
@@ -26,34 +29,60 @@ function sendReply(message, reply) {
     }
 }
 
-client.on('message', message => {
-    var validMessage = (
-        message.author.username != client.user.username
-    )
-
-    if ( validMessage ) {
-        var response
-        //Default
-        if ( hasText(message, "todd howard") ) {
-            response = getDefaultReply()
-        }
-        //Check prompts for replies
-        else {
-            for ( prompt in replies.textReplies ) {
-                if ( hasText(message, prompt) ) {
-                    response = replies.textReplies[prompt]
-                    break
+function doCommand(message) {
+    //Commands
+    if (message.content.substring(0, 1) == '?') {
+        logger.debug("Found command")
+        var args = message.content.substring(1).split(' ');
+        var thisCommand = args[0].toLowerCase();
+        const user = message.mentions.users.first()
+        if ( user ) {
+            for ( command in replies.commands ) {
+                logger.debug("Command: %s, thisCommand: %s", command, thisCommand)
+                if (command == thisCommand) {
+                    logger.debug("found command %s", command)
+                    replies.commands[command].forEach( function(reply){
+                        if (reply.startsWith('`')) {
+                            message.channel.send(eval(reply))
+                        } else {
+                            message.channel.send(reply)    
+                        }
+                        return true
+                    })
                 }
             }
         }
-        //Respond
-        if ( response != null ) {
-            response.forEach( function(reply) {
-                sendReply(message, reply)
-                logger.debug("Response sent\n\tChannel: %s\n\tMessage: %s", message.channel.name, reply)
-            })
-        }
+    }
+    return false
+}
 
+function doReplyText(message) {
+    var response
+    //Default
+    if ( hasText(message, "todd howard") ) {
+        response = getDefaultReply()
+    }
+    //Check prompts for replies
+    else {
+        for ( prompt in replies.textReplies ) {
+            if ( hasText(message, prompt) ) {
+                response = replies.textReplies[prompt]
+                break
+            }
+        }
+    }
+    //Respond
+    if ( response != null ) {
+        response.forEach( function(reply) {
+            sendReply(message, reply)
+            logger.debug("Response sent\n\tChannel: %s\n\tMessage: %s", message.channel.name, reply)
+        })
+        return true
+    }
+    return false
+}
+
+function doReplyEmoji(message) {
         //Check prompts for reactions
         for ( prompt in replies.emojiReplies ) {
             if ( hasText(message, prompt ) ) {
@@ -61,6 +90,19 @@ client.on('message', message => {
                     message.react(emoji)
                 })
             }
+        }
+}
+
+client.on('message', message => {
+    var validMessage = (
+        message.author.username != client.user.username
+    )
+
+    if ( validMessage ) {
+        const didCommand = doCommand(message)
+        if ( didCommand == false ) {
+            doReplyText(message)
+            doReplyEmoji(message)
         }
     }
 });
